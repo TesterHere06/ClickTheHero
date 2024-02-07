82d0f2abc8f5512cdc1537ec281dbd7b5559a8cc
lifehacker1337 <Artem_Gorskiy53@mail.ru>
2024-02-07 16:51:53 +0300

    New mechanics and sprites
    Added new sprites and mechanics. Play button, score, shop, upgrade shop, new platform.

---
+++ ClickTheHero/Assets/Scripts/Game.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] int Score;
+    public int[] CostInt;
+    private int ClickScore = 1;
+    public int[] CostBonus;
+
+
+    public GameObject UpgradePanel;
+    public GameObject ShopPanel;
+
+    public TextMeshProUGUI[] CostText;
+    public TextMeshProUGUI ScoreText;
+
+    private void Start()
+    {
+        StartCoroutine(BonusShop());
+    }
+
+    public void OnClickButton()
+    {
+        Score += ClickScore;
+    }
+
+    private void Update()
+    {
+        ScoreText.text = Score + "$";
+    }
+
+    public void ShowAndHideUpgradePan()
+    {
+        UpgradePanel.SetActive(!UpgradePanel.activeSelf);
+    }
+
+    public void ShowAndHideShopPan()
+    {
+        ShopPanel.SetActive(!ShopPanel.activeSelf);
+    }
+
+    public void OnClickBuyLevel()
+    {
+        if (Score >= CostInt[0])
+        {
+            Score -= CostInt[0];
+            CostInt[0] *= 2;
+            ClickScore *= 2;
+            CostText[0].text = CostInt[0] + "$";
+        }
+    }
+
+    public void OnClickBuyBonusShop()
+    {
+        if (Score >= CostInt[1])
+        {
+            Score -= CostInt[1];
+            CostInt[1] *= 2;
+            CostBonus[0] += 2;
+            CostText[1].text = CostInt[1] + "$";
+        }
+    }
+
+    IEnumerator BonusShop()
+    {
+        while(true)
+        {
+            Score += CostBonus[0];
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
